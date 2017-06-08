f735bbf60e75e2dd26b0decd855bb6db1ee7f7f4
Yang <zyterence@gmail.com>
2017-06-08 18:22:48 +0800

    get subregion urls

--- regions.py
+++ regions.py
@@ -4,27 +4,39 @@ import os, ast, json, string
 from collections import deque
 from tools.request import make_requests, make_request
 
+DONE = True
+
 def dfs_requests(urls, visited):
 	if visited is None:
 		visited = set()
 	for url in urls:
 		visited.add(url)
 		subregion_urls = get_subregion_urls(url)
-		dfs_requests(subregion_urls, visited)
+		if len(subregion_urls) > 0:
+			dfs_requests(subregion_urls, visited)
 	return visited
 
 def get_subregion_urls(url):
-	responses = make_requests([url])
+	if len(url) == 1:
+		responses = make_requests([url])
+	else:
+		return []
 	soup = None
 	for response in responses:
 		if response != None:
 			soup = response
 			subregion_urls = parse_subregions(soup)
-			return subregion_urls
+			if subregion_urls == DONE
+				return []
+			else 
+				return subregion_urls
 	return []
 
 def parse_subregions(soup):
-	return []
+	if is final:
+		return DONE
+	else:
+		return subregion_urls
 
 def test_regions():
 	print "test regions"
